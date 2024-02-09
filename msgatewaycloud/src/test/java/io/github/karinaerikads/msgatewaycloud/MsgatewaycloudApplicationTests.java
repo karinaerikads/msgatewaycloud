ee8571678a592466ca037a08205d5a920eb7070c
karinaerikads <karinaerikads48@gmail.com>
2024-02-08 21:26:56 -0300

    add dependencias

---
+++ msgatewaycloud/src/test/java/io/github/karinaerikads/msgatewaycloud/MsgatewaycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.karinaerikads.msgatewaycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgatewaycloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
